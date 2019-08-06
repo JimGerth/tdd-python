c54b52732b28ab04428c5994e2c8aa912fdd0601
JimGerth <jim.frederic.gerth@gmail.com>
2019-08-06 15:53:45 +0200

    added addition method to bank, which first converts the arguments to the wanted currency and then passes the money objects to its calculator to add up. also the exchange rate "data base" now directly also stores the inverse of an exchange rate which makes usage more convienient

--- bank.py
+++ bank.py
@@ -1,9 +1,11 @@
+from calculator import Calculator
 from money import Money
 
 class Bank:
 
     def __init__(self):
         self.rates = {}
+        self.calc = Calculator()
 
     def reduce(self, expression, currency):
         try:
@@ -14,10 +16,18 @@ class Bank:
 
     def addRate(self, currency1, currency2 , rate):
         self.rates[(currency1, currency2)] = rate
+        self.rates[(currency2, currency1)] = 1 / rate
 
     def rate(self, currency1, currency2):
         if currency1 == currency2: return 1
         return self.rates[(currency1, currency2)]
 
     def convert(self, money, to):
+        if money.currency == to:
+            return money
         return Money(money.amount / self.rate(money.currency, to), to)
+
+    def add(self, money1, money2, currency=None):
+        if not currency:
+            currency = money1.currency
+        return self.calc.add(self.convert(money1, currency), self.convert(money2, currency))
--- bank_test.py
+++ bank_test.py
@@ -16,17 +16,18 @@ class BankTest(unittest.TestCase):
         bank = Bank()
         bank.addRate('CHF', 'USD', 2)
         self.assertTrue(bank.convert(Money.franc(10), 'USD') == Money.dollar(5))
+        self.assertTrue(bank.convert(Money.dollar(5), 'CHF') == Money.franc(10))
 
     def test_simple_addition(self):
-        self.assertTrue(Bank().reduce(Money.dollar(5) + Money.dollar(5), 'USD') == Money.dollar(10))
+        self.assertTrue(Bank().add(Money.dollar(5), Money.dollar(5)) == Money.dollar(10))
 
     def test_mixed_addition(self):
         bank = Bank()
         bank.addRate('CHF', 'USD', 2)
-        self.assertTrue(bank.reduce(Money.dollar(5) + Money.franc(10), 'USD') == Money.dollar(10))
+        self.assertTrue(bank.add(Money.dollar(5), Money.franc(10), 'USD') == Money.dollar(10))
 
     def test_multiple_addition(self):
-        self.assertTrue(Bank().reduce(Money.dollar(5) + Money.dollar(10) + Money.dollar(15), 'USD') == Money.dollar(30))
+        self.assertTrue(Bank().add(Bank().add(Money.dollar(5), Money.dollar(10)), Money.dollar(15)) == Money.dollar(30))
 
     def test_simple_multiplication(self):
         self.assertTrue(Bank().reduce(Money.franc(10) * 2, 'CHF') == Money.franc(20))
