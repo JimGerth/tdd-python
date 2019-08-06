f03fd2c8096c7d5fdc19fa5cf95e140d35016608
JimGerth <jim.frederic.gerth@gmail.com>
2019-08-06 16:08:58 +0200

    removed __add__, __mul__ and reduce methods from money class, thus eliviating it from those responsibilities

--- money.py
+++ money.py
@@ -18,18 +18,8 @@ class Money:
     def __eq__(self, money):
         return self.amount == money.amount and self.currency == money.currency
 
-    def __add__(self, addend):
-        from sum import Sum
-        return Sum(self, addend)
-
-    def __mul__(self, multiplier):
-        return Money(self.amount * multiplier, self.currency)
-
     def dollar(amount):
         return Money(amount, 'USD')
 
     def franc(amount):
         return Money(amount, 'CHF')
-
-    def reduce(self, bank, currency):
-        return Money(self.amount / bank.rate(self.currency, currency), currency)
